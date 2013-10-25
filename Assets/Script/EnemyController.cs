6dd69c06f56732df8cec4a61e70cb42db5e174b1
arnaud_m <nessdu43@hotmail.fr>
2013-10-25 21:42:02 +0200

    mise en place du pathfinder v0 les nenemis ne sont pas tout a fait optimisés

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour 
+{
+	private Transform target;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetComponent<NavMeshAgent>().destination = target.transform.position;
+	}
+}
