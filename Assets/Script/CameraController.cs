eee4abd8cd7a73bd2a9080a73582fb2ab62953ad
Geoffrey C <gclauzier@gmail.com>
2013-10-25 16:00:40 +0200

    Début de mouvement de caméra à la souris : la caméra tourne en fonction des déplacements de la souris, il reste encore à la faire tourner autour du personnage. Ajout d'un son de pas pour les déplacements du joueur.

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -6,13 +6,50 @@ public class CameraController : MonoBehaviour {
 	public GameObject target;
 	private Vector3 offset;
 	
+	// Mouvement du perso avec la souris
+	private float mouseX;
+	private float mouseY;
+	public float mouseSpeed = 10f;
+	
 	// Use this for initialization
 	void Start () {
 		offset = transform.position;
+		// Initialisation de la position de la souris
+		Input.mousePosition.Set(0,0,0);
+		mouseX = Input.mousePosition.x;
+		mouseY = Input.mousePosition.y;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
 		transform.position = target.transform.position + offset;
+		
+		handleMouseRotation();
+	}
+	
+	
+	void handleMouseRotation()
+	{
+		float positionX = Input.mousePosition.x;
+		float positionY = Input.mousePosition.y;
+		float deltaX, deltaY;
+		if (positionX != mouseX)
+		{
+			deltaX = (positionX - mouseX) * mouseSpeed * Time.deltaTime;
+			mouseX = positionX;
+		}
+		else
+			deltaX = 0;
+		
+		
+		if (positionY != mouseY)
+		{
+			deltaY = (positionY - mouseY) * mouseSpeed * Time.deltaTime;
+			mouseY = positionY;
+		}
+		else
+			deltaY = 0;
+		
+		this.transform.Rotate(-deltaY, deltaX, 0);
 	}
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -11,10 +11,18 @@ public class PlayerController : MonoBehaviour {
 	
 	private CharacterController controller;
 	
+	// Mouvement du perso avec la souris
+	private float mouseX;
+	public float mouseSpeed = 10f;
+	
 	// Use this for initialization
 	void Start () {
 		gameObject.renderer.material.color = new Color(255, 0, 0);
 		controller = GetComponent<CharacterController>();
+		
+		// Initialisation de la position de la souris
+		Input.mousePosition.Set(0,0,0);
+		mouseX = Input.mousePosition.x;
 	}
 	
 	// Update is called once per frame
@@ -33,7 +41,23 @@ public class PlayerController : MonoBehaviour {
 		// Applies move
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
-
+		//handleMouseRotation();
+	}
+	
+	void handleMouseRotation()
+	{
+		float positionX = Input.mousePosition.x;
+		float deltaX;
+		if (positionX != mouseX)
+		{
+			deltaX = (positionX - mouseX) * mouseSpeed * Time.deltaTime;
+			mouseX = positionX;
+		}
+		else
+			deltaX = 0;
+		
+		this.transform.Rotate(0, 0, deltaX);
+		
 	}
 	
 	void OnTriggerEnter (Collider other)
