80132a33ff338df5ee76a12e8d35e10b4775f8ca
Geoffrey C <gclauzier@gmail.com>
2013-10-25 20:00:12 +0200

    Renommage d'un fichier. Ajout du tag weapon. Ajout de vie et d'XP au joueur. Ajout de la gestion de la vie et de l'XP.

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -11,28 +11,42 @@ public class PlayerController : MonoBehaviour {
 	
 	private CharacterController controller;
 	
+	private int maxpv=200, pv=200, xp=0;
+	public GUIText healthText;
+	public GUIText experienceText;
+	public GUIText gameOverText;
+	private bool gameOver = false;
+	
 	// Use this for initialization
 	void Start () {
 		gameObject.renderer.material.color = new Color(255, 0, 0);
 		controller = GetComponent<CharacterController>();
+		experienceText.color = new Color(0,0,255);
+		statuUpdate();
+		gameOverText.enabled=false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-        if (controller.isGrounded) {
-			// Moves forward, left, right, backward
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-			// Handle jumps
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
-            
-        }
-		// Applies move
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+		if (!gameOver)
+		{
+	        if (controller.isGrounded) {
+				// Moves forward, left, right, backward
+	            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	            moveDirection = transform.TransformDirection(moveDirection);
+	            moveDirection *= speed;
+				// Handle jumps
+	            if (Input.GetButton("Jump"))
+	                moveDirection.y = jumpSpeed;
+	        }
+			// Applies move
+	        moveDirection.y -= gravity * Time.deltaTime;
+	        controller.Move(moveDirection * Time.deltaTime);
+		}
+		else
+		{
+			gameOverText.enabled=true;
+		}
 	}
 	
 	
@@ -43,6 +57,52 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.tag == "Item")
 		{
 			other.gameObject.SetActive(false);
+			healthUpdate(50);
 		}
+		if (other.gameObject.tag == "Weapon")
+		{
+			other.gameObject.SetActive(false);
+			healthUpdate(-50);
+			experienceUpdate(40);
+		}
+	}
+	
+	void healthUpdate(int change)
+	{
+		pv += change;
+		statuUpdate();
+	}
+	
+	void experienceUpdate(int change)
+	{
+		xp += change;
+		statuUpdate();
+	}
+	
+	void statuUpdate()
+	{
+		if (pv<=0)
+		{
+			healthText.color = new Color(255,0,0);
+        	healthText.text = "0";
+			gameOver = true;
+		}
+        else if (pv<= maxpv * 1/4)
+		{
+			healthText.color = new Color(255,0,0);
+			healthText.text = pv.ToString();
+		}
+		else if (pv<= maxpv * 3/4)
+		{
+			healthText.color = new Color(125,125,0);
+			healthText.text = pv.ToString();
+		}
+		else
+		{
+			healthText.color = new Color(0,255,0);
+			healthText.text = pv.ToString();
+		}
+	
+		experienceText.text = xp.ToString();
 	}
 }
