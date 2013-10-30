82d094cb0f4674a0d8726cce73ad67e75d056b79
Gaillard <t_of_43@hotmail.fr>
2013-10-30 13:11:30 +0100

    Ajout du Menu Pause: -La touche "Echap" lance le menu pause et stoppe le temps -Le curseur de la souris est caché en jeu et apparait en Pause Et c'est trop cooooooooooool !!!

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+	
+	
+	public bool paused;
+	
+	
+	void Start() // le jeu n'est pas en pause au départ
+	{
+		paused = false;
+	}
+	
+	
+	void Update()
+	{
+		
+		if(paused)	// actualise la valeur du timeScale selon si "En Pause"
+		{
+			Time.timeScale = 0;
+			
+			// on fait apparaitre le curseur
+			Screen.showCursor = true;
+			Screen.lockCursor = false;
+		}
+		else 		// ou pas
+		{
+			Time.timeScale = 1;
+			
+			// le cursor ne peux plus bouger et est caché
+			Screen.showCursor = false;
+			Screen.lockCursor = true;
+		}
+		
+		
+		// si on appuie sur "Escape" change l'état du jeu
+		if(Input.GetKeyDown(KeyCode.Escape))
+			paused = !paused;
+	}
+	
+	
+	
+	void OnGUI()
+	{
+		if(paused)
+		{
+			Pause ();
+		}
+	}
+	
+	
+	void Pause()
+	{
+		GUILayout.BeginArea(new Rect(Screen.width/2-50,Screen.height/2-50, 100,100));
+		
+		if(GUILayout.Button("Continuer"))
+		{
+			paused = false;
+		}
+		if(GUILayout.Button("Menu"))
+		{
+			// Renvoie au menu
+		}
+		
+		GUILayout.EndArea();
+	}
+	
+}
