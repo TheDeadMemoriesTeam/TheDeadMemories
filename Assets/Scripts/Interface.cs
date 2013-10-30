743800e49f4ee90e11c6fba37da845706a0e145e
Geoffrey C <gclauzier@gmail.com>
2013-10-30 15:01:37 +0100

    Décommente deux lignes de code qui ne bug pas

--- Assets/Scripts/Interface.cs
+++ Assets/Scripts/Interface.cs
@@ -16,7 +16,7 @@ public class Interface : MonoBehaviour
 	void Start () 
 	{
 		experienceText.color = new Color(0,0,255);
-		//manaText.color = Color.magenta;
+		manaText.color = Color.magenta;
 		gameOverText.enabled=false;
 		healthTextureEmpty.pixelInset = new Rect(-32,Screen.height-32,32,32); // position de la texture vide de la barre de vie
 		healthTexture.pixelInset = new Rect(-32,Screen.height-32,32,32); // position de la texture de la barre de vie actuelle du joueur
@@ -41,6 +41,6 @@ public class Interface : MonoBehaviour
 		healthTexture.border = new RectOffset(hpBarPxl,0,0,0);
 	
 		experienceText.text = player.getExperience().ToString();
-		//manaText.text = player.getMana().ToString() + " / " + player.getManaMax().ToString();
+		manaText.text = player.getMana().ToString() + " / " + player.getManaMax().ToString();
 	}
 }
