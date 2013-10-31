901a37e56152e09c8545183944f4f9bdbd16282f
Alexandre_Raberin <kernelith@live.fr>
2013-10-31 18:29:36 +0100

    Ajout d'une série d'achievements lié à la distance parcourut !

--- Assets/Scripts/AchivementManager.cs
+++ Assets/Scripts/AchivementManager.cs
@@ -31,6 +31,13 @@ public class AchivementManager : MonoBehaviour {
 	
 	private bool longArm = false;
 	
+	private bool oneKilometer = false;
+	private bool tenKilometers = false;
+	private bool marathon = false;
+	private bool hundredKilometers = false;
+	private bool thousandKilometers = false;
+	private bool milionKilometer = false;
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -188,6 +195,66 @@ public class AchivementManager : MonoBehaviour {
 			Debug.Log("Achivement Long Arm (10 kills in the same time) !");
 			unlockAchivement(texture);
 			longArm = !longArm;
+		}
+	}
+	
+	public void sundayWalkerAchievement()
+	{
+		if (!oneKilometer)
+		{
+			Debug.Log("Achivement Sunday Walker (run on 1 km) !");
+			unlockAchivement(texture);
+			oneKilometer = !oneKilometer;
+		}
+	}
+	
+	public void dailyJoggingAchievement()
+	{
+		if (!tenKilometers)
+		{
+			Debug.Log("Achivement Daily Jogging (run on 10 km) !");
+			unlockAchivement(texture);
+			tenKilometers = !tenKilometers;
+		}
+	}
+	
+	public void marathonAchievement()
+	{
+		if (!marathon)
+		{
+			Debug.Log("Achivement Marathon (run on 42,195 km) !");
+			unlockAchivement(texture);
+			marathon = !marathon;
+		}
+	}
+	
+	public void healthWalkAchievement()
+	{
+		if (!hundredKilometers)
+		{
+			Debug.Log("Achivement Health Walk (run on 100 km) !");
+			unlockAchivement(texture);
+			hundredKilometers = !hundredKilometers;
+		}
+	}
+	
+	public void athleticAchievement()
+	{
+		if (!thousandKilometers)
+		{
+			Debug.Log("Achivement Athletic (run on 1.000 km) !");
+			unlockAchivement(texture);
+			thousandKilometers = !thousandKilometers;
+		}
+	}
+	
+	public void dopedAddictAchievement()
+	{
+		if (!milionKilometer)
+		{
+			Debug.Log("Achivement Doped Addict (run on 1.000.000 km) !");
+			unlockAchivement(texture);
+			milionKilometer = !milionKilometer;
 		}
 	}
 	
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,11 +15,13 @@ public class PlayerController : HumanoidController
 	
 	private int xp=0;
 	
+	// Variables servants aux achievements
 	public AchivementManager achivementManager;
 	private int cptEnemyKilled = 0;
 	private float timeNotTouched = 0;
 	private float timeSurvived = 0;
 	private bool assassin = true;
+	private float travel = 0;
 	
 	// Use this for initialization
 	void Start () 
@@ -55,8 +57,10 @@ public class PlayerController : HumanoidController
         }
 		// Applies move
         moveDirection.y -= gravity * Time.deltaTime;
+		Vector3 vec = transform.position;
         controller.Move(moveDirection * Time.deltaTime);
-		
+		updateTravel(vec, transform.position);
+
 		// Rotation
 		rotation = new Vector3(0, Input.GetAxis("Mouse X"), 0);
 		rotation *= rotationFactor * Time.timeScale;
@@ -140,6 +144,24 @@ public class PlayerController : HumanoidController
 	{
 		timeNotTouched = time;
 		assassin = false;
+	}
+	
+	private void updateTravel(Vector3 fromWhere, Vector3 to)
+	{
+		travel += Vector3.Distance(fromWhere, to);
+		
+		if (travel >= 1000)		// 1 km parcourut
+			achivementManager.sundayWalkerAchievement();
+		if (travel >= 10000)	// 10 km parcourut
+			achivementManager.dailyJoggingAchievement();
+		if (travel >= 42195)	// 42,195 km parcourut
+			achivementManager.marathonAchievement();
+		if (travel >= 100000)	// 100 km parcourut
+			achivementManager.healthWalkAchievement();
+		if (travel >= 1000000)	// 1.000 km parcourut
+			achivementManager.athleticAchievement();
+		if (travel >= 1000000000)// 1.000.000 km parcourut
+			achivementManager.dopedAddictAchievement();
 	}
 	
 	private void timedAchievements()
