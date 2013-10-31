3471c3a69e44f86fc9a12ae58e04b2bef16c7668
elestival <eloestiv@gmail.com>
2013-10-31 11:26:52 +0100

    Menu principal

---
+++ Assets/Scripts/TextControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextControl : MonoBehaviour {
+	
+	public bool isQuitButton=false;
+	
+	void OnMouseEnter() {
+		renderer.material.color = Color.red;
+	}
+	
+	void OnMouseExit() {
+		renderer.material.color = Color.white;
+	}
+	
+	void OnMouseUp()
+    {
+       if(isQuitButton)
+       {
+       		Application.Quit();  
+       }
+       else
+       {
+       		 //Application.LoadLevel(1);
+ 
+       }
+    }
+}
+
