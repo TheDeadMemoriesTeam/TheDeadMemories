9b9abe95852330035b5915a9c2388dfdece62ce5
arnaud_m <nessdu43@hotmail.fr>
2013-11-06 21:22:38 +0100

    changement du menu mise en place du terrain fait par tibo :)

--- Assets/Scripts/MenuItemControl.cs
+++ Assets/Scripts/MenuItemControl.cs
@@ -5,6 +5,17 @@ public class MenuItemControl : MonoBehaviour
 {
 	
 	public bool isQuitButton = false;
+	public bool isPlayButton = false;
+	public bool isNewButton = false;
+	public bool isLoadButton = false;
+	public bool isReturn = false;
+	
+	private CameraMenu cam;
+		
+	void Start()
+	{
+		cam = FindObjectOfType(System.Type.GetType("CameraMenu")) as CameraMenu;
+	}
 	
 	void OnMouseEnter() 
 	{
@@ -18,15 +29,30 @@ public class MenuItemControl : MonoBehaviour
 	
 	void OnMouseUp()
     {
-       if(isQuitButton)
-       {
-       		Application.Quit();  
-       }
-       else
-       {
+		if(isQuitButton)
+			Application.Quit();
+		
+		if(isPlayButton)
+		{	
+			cam.dest = new Vector3(25.4f, 1.5f, 19.5f);
+			return;
+		}
+		
+		if(isNewButton)
        		 Application.LoadLevel(1);
- 
-       }
+ 		
+		if(isLoadButton)
+		{
+			//plus tard
+			return;
+		}
+		
+		if(isReturn)
+		{
+			cam.dest = new Vector3(20f, 1.5f, 5f);
+		}
     }
+	//20 1.5 5
+	//25.4 1.5 19.5
 }
 
