1e8a247c48ca5e2381dd5a63e8722cb2906dadad
arnaud_m <nessdu43@hotmail.fr>
2013-11-08 00:45:00 +0100

    bon jais ajouter un step a la cam mais jais chercher comment je pouvais faire pour quil arrive a la bonne position la c aproximatif et c chaint je bidoullerais ca

--- Assets/Scripts/CameraMenu.cs
+++ Assets/Scripts/CameraMenu.cs
@@ -6,26 +6,57 @@ public class CameraMenu : MonoBehaviour
 	public float smooth = 0.1f;
 	
 	private Vector3 dest;
+	private Vector3 step;
+	
+	private float time;
+	
+	private bool translate;
+	
 	// Use this for initialization
 	void Start () 
 	{
 		dest = transform.position;
+		step = transform.position;
+		
+		time = 0;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.position = Vector3.Lerp(transform.position, dest, smooth * Time.deltaTime);
+		if(translate)
+		{
+			time += Time.deltaTime;
+			if(time>0.5)
+			{
+				transform.position = Vector3.Lerp(transform.position, dest, smooth * Time.deltaTime);
+			}
+			else
+			{
+				transform.position = Vector3.Lerp(transform.position, step, smooth * Time.deltaTime);
+			} 
+			if((int)transform.position.magnitude - (int)dest.magnitude == 0)
+			{
+				translate = false;
+				time = 0;
+			}
+		}
 	}
 
 	
 	public void goToMainMenu()
 	{
+		translate = true;
+		step = new Vector3(22.7f, 5f, 12.25f);
 		dest = new Vector3(20f, 1.5f, 5f);
+		//20 1.5 5
+	//25.4 1.5 19.5
 	}
 	
 	public void goToLaunchMenu()
 	{
+		translate = true;
+		step = new Vector3(22.7f, 5f, 12.25f);
 		dest = new Vector3(25.4f, 1.5f, 19.5f);
 	}
 }
