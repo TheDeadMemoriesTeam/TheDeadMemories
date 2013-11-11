cdb9c56ebcca5f96828787a584edf5b34fa0527c
Alexandre_Raberin <kernelith@live.fr>
2013-11-11 22:06:36 +0100

    Ajoute la famille d'achievement kill x ennemis en 1 min (1ère version, peut être définitive)

---
+++ Assets/Scripts/Achievements/TimedKillAchievement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedKillAchievement : Achievement
+{
+	protected int requiredKills;
+	protected float timeGive;
+	
+	public TimedKillAchievement(AchievementManager am, string name, string description, int requiredKills, float timeGive)
+		: base(am, name, description)
+	{
+		this.requiredKills = requiredKills;
+		this.timeGive = timeGive;
+	}
+	
+	public override bool achieved()
+	{
+		if (!(timeGive - am.getLimitTime() >= 0))
+		{
+			am.resetKillPerSession();
+			return false;
+		}
+		return am.getNbKillPerSession() >= requiredKills;
+	}
+}
