70e1afa6bbdb2c89fdc4ba579a2ec422a5fd1e42
arnaud_m <nessdu43@hotmail.fr>
2013-11-14 00:19:07 +0100

    debut de la mise en place de l'arbre des competance

---
+++ Assets/Scripts/Skill/PassiveSkills.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PassiveSkills : Skills 
+{
+	private int lvlSkill = 0;
+	
+	//acsessor
+	public void setLvlSkill(int lvl)
+	{
+		lvlSkill = lvl;	
+	}
+	
+	public int getLvlSkill()
+	{
+		return lvlSkill;	
+	}
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
