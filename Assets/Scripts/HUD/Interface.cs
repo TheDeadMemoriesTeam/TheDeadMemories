dc3327bafff50a9bb9a23bea64666c331e257d2a
Gaillard <t_of_43@hotmail.fr>
2013-12-13 13:11:58 +0100

    Coucou ! Barre de vie et de mana et c'est tout beau tout joli !!!

--- Assets/Scripts/HUD/Interface.cs
+++ Assets/Scripts/HUD/Interface.cs
@@ -8,27 +8,27 @@ public class Interface : MonoBehaviour
 	
 	public GUIText gameOverText;
 	public float lifeShaker;
-	public GUITexture healthTextureEmpty;
-	public GUITexture healthTextureEmpty2;
-	public GUITexture healthTexture;
-	public GUITexture healthTexture2;
 
 	public GUITexture healthManaTextureEmpty;
-	
+
+	public GUITexture ManaTexture;
+	public GUITexture VieTexture;
+
 	// Use this for initialization
 	void Start ()
 	{
 		gameOverText.enabled=false;
 		
 		lifeShaker = 0;
-		
-		healthTextureEmpty.pixelInset = new Rect(-64,Screen.height-32,64,32); // position de la texture vide de la barre de vie
-		healthTextureEmpty2.pixelInset = new Rect(-64,Screen.height-32,64,32); // position de la texture vide de la barre de vie
-		healthTexture.pixelInset = new Rect(-64,Screen.height-32,64,32); // position de la texture de la barre de vie actuelle du joueur
-		healthTexture2.pixelInset = new Rect(-64,Screen.height-32,64,32); // position de la texture de la barre de vie actuelle du joueur
 
 		healthManaTextureEmpty.pixelInset = new Rect(Screen.width/2-260, -256, 521, 256);
 		healthManaTextureEmpty.border = new RectOffset(0,0,0,511);
+
+		ManaTexture.pixelInset = new Rect(Screen.width/2-260, -256, 521, 256);
+		ManaTexture.border = new RectOffset(0,0,0,511);
+		
+		VieTexture.pixelInset = new Rect(Screen.width/2-260, -256, 521, 256);
+		VieTexture.border = new RectOffset(0,0,0,511);
 
 	}
 	
@@ -52,58 +52,18 @@ public class Interface : MonoBehaviour
 		
 		
 		// actualisation de la barre de vie
-		double hpBar = (double)hp * (256/(double)hpMax);
-		int hpBarPxl = (int)hpBar + 64;
+		double hpBar = (double)hp * (129/(double)hpMax);
+		int hpBarPxl = (int)hpBar + 284;
 		
-		if((double)hp/(double)hpMax <= 0.25)
-		{
-			Rect rec1 = new Rect(-64,Screen.height-33,64,32);
-			Rect rec2 = new Rect(-65,Screen.height-32,64,32);
-			Rect rec3 = new Rect(-64,Screen.height-32,64,32);
-			Rect rec4 = new Rect(-65,Screen.height-33,64,32);
-			lifeShaker += Time.deltaTime;
-			if(lifeShaker >= 0.4)
-			{
-				healthTextureEmpty.pixelInset = rec1; // position de la texture vide de la barre de vie
-				healthTextureEmpty2.pixelInset = rec2; // position de la texture vide de la barre de vie
-				healthTexture.pixelInset = rec1; // position de la texture de la barre de vie actuelle du joueur
-				healthTexture2.pixelInset = rec2; // position de la texture de la barre de vie actuelle du joueur
-				lifeShaker = 0;
-			}
-			else if(lifeShaker >= 0.3)
-			{
-				healthTextureEmpty.pixelInset = rec2; // position de la texture vide de la barre de vie
-				healthTextureEmpty2.pixelInset = rec3; // position de la texture vide de la barre de vie
-				healthTexture.pixelInset = rec2; // position de la texture de la barre de vie actuelle du joueur
-				healthTexture2.pixelInset = rec3; // position de la texture de la barre de vie actuelle du joueur
-			}
-			else if(lifeShaker >= 0.2)
-			{
-				healthTextureEmpty.pixelInset = rec3; // position de la texture vide de la barre de vie
-				healthTextureEmpty2.pixelInset = rec4; // position de la texture vide de la barre de vie
-				healthTexture.pixelInset = rec3; // position de la texture de la barre de vie actuelle du joueur
-				healthTexture2.pixelInset = rec4; // position de la texture de la barre de vie actuelle du joueur
-			}
-			else if(lifeShaker >= 0.1)
-			{
-				healthTextureEmpty.pixelInset = rec4; // position de la texture vide de la barre de vie
-				healthTextureEmpty2.pixelInset = rec1; // position de la texture vide de la barre de vie
-				healthTexture.pixelInset = rec4; // position de la texture de la barre de vie actuelle du joueur
-				healthTexture2.pixelInset = rec1; // position de la texture de la barre de vie actuelle du joueur
-			}
-		}
-		
-		healthTextureEmpty.border = new RectOffset(320,0,0,0);
-		healthTextureEmpty2.border = new RectOffset(320,0,0,0);
-		healthTexture.border = new RectOffset(hpBarPxl,0,0,0);
-		healthTexture2.border = new RectOffset(hpBarPxl,0,0,0);
+		VieTexture.border = new RectOffset(0,0,0,hpBarPxl);
 		
 		
 		
 		// actualisation de la barre de mana
-		double manaBar = (double)mana * (256/(double)manaMax);
-		int manaBarPxl = (int)manaBar + 256;
+		double manaBar = (double)mana * (132/(double)manaMax);
+		int manaBarPxl = (int)manaBar + 288;
 		
-		
+		ManaTexture.border = new RectOffset(0,0,0,manaBarPxl);
+
 	}
 }
