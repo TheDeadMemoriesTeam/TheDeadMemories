1bc216fea5c17c3161395993b4d5a85f17ef768a
elestival <elodie.estival@etu.udamail.fr>
2013-12-28 20:05:24 +0100

    Amélioration du menu de pause

--- Assets/Scripts/HUD/PauseMenu.cs
+++ Assets/Scripts/HUD/PauseMenu.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class PauseMenu : PauseSystem
 {
-	
+
+	public GUITexture pausePanel;
+	public GUIStyle buttonGUIStyle;
+	public Font buttonFont;
+
 	protected override void Start()
 	{
 		base.Start();
+		//on cache le fond du menu pause
+		pausePanel.pixelInset=new Rect(100, -64, 256, 64);
 	}
 	
 	
@@ -20,23 +26,39 @@ public class PauseMenu : PauseSystem
 				paused = true;
 				
 				UpdateState();
+
+				pausePanel.pixelInset = new Rect(0, 0, Screen.width, Screen.height);
+
 			}
 		}
 	}
-	
+
+
 	void OnGUI()
 	{
+		buttonGUIStyle = new GUIStyle();
+		buttonGUIStyle.normal.textColor = Color.white;
+		buttonGUIStyle.fontStyle=FontStyle.Bold;
+		buttonGUIStyle.fontSize = 40;  
+		buttonGUIStyle.font = buttonFont;
+
 		if(paused)
 		{
-			GUILayout.BeginArea(new Rect(Screen.width/2-50,Screen.height/2-50, 100,100));
 			
-			if(GUILayout.Button("Continuer"))
+
+
+			GUILayout.BeginArea(new Rect(Screen.width/2-50,Screen.height/2-50, 200,200));
+			
+			if(GUILayout.Button("Continuer", buttonGUIStyle))
 			{
 				paused = false;
 				
 				UpdateState();
+
+				//on cache le fond du menu pause
+				pausePanel.pixelInset=new Rect(100, -64, 256, 64);
 			}
-			if(GUILayout.Button("Menu"))
+			if(GUILayout.Button("Menu", buttonGUIStyle))
 			{
 				player.achievementManager.saveAchievements();
 				Application.LoadLevel(0);
